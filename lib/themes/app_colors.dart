531dadc1698ff6af292040dd1f9bc9482f26a2da
NguyenTanPhucK11 <16520954@gm.uit.edu.vn>
2021-03-29 19:58:09 +0700

    init color & text style

---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color activeColor = Color(0xff22A45D);
+  static const Color backgroundColor = Color(0xffffffff);
+  static const Color accentColor = Color(0xffEF9920);
+
+  static const Color mainText = Color(0xff010F07);
+  static const Color bodyText = Color(0xff868686);
+  static const Color inputColor = Color(0xff22A45D);
+
+  static const Color googleColor = Color(0xff4285F4);
+  static const Color facebookColor = Color(0xff395998);
+
+}
---
+++ lib/themes/text_style.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class FontFamily {
+  static String fontSF = '';
+}
+
+class StyleText {
+  static TextStyle title1 = TextStyle(
+    fontSize: 34,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle title2 = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle title3 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle headline = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle subhead = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle body = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+  static TextStyle caption = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+}
